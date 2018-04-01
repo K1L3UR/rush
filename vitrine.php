ed07c43a65be39c089b69af983c2dc577cd188b2
Arnaud DURAND <ardurand@e2r11p5.42.fr>
2018-04-01 23:43:13 +0200

    popo

--- vitrine.php
+++ vitrine.php
@@ -10,41 +10,41 @@ session_start();
 <p id="bienvenue">Bienvenue <?php echo $_SESSION['login'] ?> !</p>
 <div id="produits">
 	<h1 id="sales">Nutella</h1>
-	<img src"=http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="https://www.static.ferrero.com/globalcms/immagini/27466.jpg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Confiture</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="https://www.bonnemaman.ch/uploads/catalogues_price_image/confiturePackaging-fraise.jpg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Cacahuete</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="https://static.greenweez.com/images/products/3000/600/rapunzel-beurre-de-cacahuete-bio.jpg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Beurre</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="https://www.paysanbreton.com/sites/default/files/formats/beurre-moule-demi-sel.png" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Recettes</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="http://images.media-allrecipes.com/userphotos/250x250/34761.jpg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Moutarde</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="https://279173f3.nuajik.io/50-large_default/frenchs-classic-yellow-mustard.jpg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Haricots</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="https://target.scene7.com/is/image/Target/13742542?wid=520&hei=520&fmt=pjpeg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Oignons</h1>
-	<img src="grjkndhgkrwlgbhekwtjerkhrithgwejitweruwerhwerwrkhwekrhwerkwherkwhkwehrkew" width="192" height="170">
+	<img src="http://www.bestgrocerydelivery.net/assets/images/frenchsfriedonions.jpg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Brioches</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="http://279173f3.nuajik.io/27-large_default/betty-crocker-wild-blueberry-muffin-mix.jpg" width="192" height="170">
 </div>
 <div id="produits">
 	<h1 id="sales">Café</h1>
-	<img src="http://seriousstartups.com/wp-content/uploads/2016/04/Dry-Foods-comp.jpg" width="192" height="170">
+	<img src="http://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_7d3201e2-bed0-43b4-81e6-5afb573c9758.jpg" width="192" height="170">
 </div>
